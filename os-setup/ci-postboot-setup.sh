28a5bfb3b71c0453e52af392fd60e545a05cacc7
Ethan Li <lietk12@gmail.com>
2024-05-26 01:02:51 -0700

    Troubleshoot running of setup scripts during boot

--- os-setup/ci-postboot-setup.sh
+++ os-setup/ci-postboot-setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -eux
+# This script must be run as root.
 
 # This is needed to have the login prompt on tty1, because we disabled userconfig.service. See
 # https://forums.raspberrypi.com/viewtopic.php?p=2032694#p2032694
-sudo systemctl enable getty@tty1
+systemctl enable getty@tty1
--- os-setup/ci-preboot-setup.sh
+++ os-setup/ci-preboot-setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -eux
+# This script must be run as root.
 
 # Change default settings for the SD card to enable headless & keyboardless first boot
 # Note: we could change the username by making a `/boot/userconf.txt` file with the new username
@@ -10,6 +11,6 @@
 # https://github.com/raspberrypi/rpi-imager/blob/qml/src/OptionsPopup.qml and
 # the RPi SD card image's `/usr/lib/raspberrypi-sys-mods/firstboot` and
 # `/usr/lib/raspberrypi-sys-mods/imager_custom` scripts
-echo "pi:copepode" | sudo chpasswd
-sudo sed -i -e "s~^XKBLAYOUT=.*~XKBLAYOUT=\"us\"~" /etc/default/keyboard
-sudo systemctl disable userconfig.service
+echo "pi:copepode" | chpasswd
+sed -i -e "s~^XKBLAYOUT=.*~XKBLAYOUT=\"us\"~" /etc/default/keyboard
+systemctl disable userconfig.service
