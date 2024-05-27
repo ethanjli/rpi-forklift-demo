2048de30236d2802a23396ee508df8b4ab36ad1a
Ethan Li <lietk12@gmail.com>
2024-05-26 22:03:17 -0700

    Try repairing OS image filesystem between manipulations

--- tools/mount-image.sh
+++ tools/mount-image.sh
@@ -5,6 +5,7 @@ image="$1"
 sysroot="$2"
 
 device="$(losetup -fP --show "$image")"
+e2fsck -p -f "${device}p2"
 mkdir -p "$sysroot"
 mount "${device}p2" "$sysroot"
 mount "${device}p1" "$sysroot/boot"
--- tools/unmount-image.sh
+++ tools/unmount-image.sh
@@ -6,4 +6,5 @@ sysroot="$2"
 
 umount "$sysroot/boot"
 umount "$sysroot"
+e2fsck -p -f "${device}p2"
 losetup -d "$device"
