5ece9c25dd1328ed6e41f7dc11a5dfeb013cab9c
Ethan Li <lietk12@gmail.com>
2024-05-27 18:05:55 -0700

    Troubleshoot custom dtb creation

--- tools/build-custom-dtb.sh
+++ tools/build-custom-dtb.sh
@@ -2,16 +2,19 @@
 # See https://forums.raspberrypi.com/viewtopic.php?p=2207807#p2207807
 
 base_dtb="$1"
-custom_dtb="$2"
+output_dtb="$2"
 
-cp "$base_dtb" "$custom_dtb"
+intermediate_dtb="$(mktemp --tmpdir=/tmp dtb.XXXXXXX)"
+cp "$base_dtb" "$intermediate_dtb"
 
 # dtparam=uart0=on
-tmpfile="$(mktemp --tmpdir=/tmp dtb.XXXXXXX)"
-dtmerge "$custom_dtb" "$tmpfile" - uart0=on
-mv "$tmpfile" "$custom_dtb"
+tmpfile="$(mktemp -u --tmpdir=/tmp dtb.XXXXXXX)"
+dtmerge "$intermediate_dtb" "$tmpfile" - uart0=on
+mv "$tmpfile" "$intermediate_dtb"
 
 # dtparam=disable-bt
-tmpfile="$(mktemp --tmpdir=/tmp dtb.XXXXXXX)"
-dtmerge "$custom_dtb" "$tmpfile" /boot/overlays/disable-bt.dtbo
-mv "$tmpfile" "$custom_dtb"
+tmpfile="$(mktemp -u --tmpdir=/tmp dtb.XXXXXXX)"
+dtmerge "$intermediate_dtb" "$tmpfile" /boot/overlays/disable-bt.dtbo
+mv "$tmpfile" "$intermediate_dtb"
+
+cp "$intermediate_dtb" "$output_dtb"
