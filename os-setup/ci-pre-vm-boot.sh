b0d48984d8591f8a8fcc3590939ccd75c6c93984
Ethan Li <lietk12@gmail.com>
2024-05-29 12:06:56 -0700

    See what happens if we don't do the pre-headless-boot setup before booting the VM

--- os-setup/ci-pre-vm-boot.sh
+++ os-setup/ci-pre-vm-boot.sh
@@ -9,7 +9,3 @@ systemctl enable vm-boot-setup@$setup_user.service
 
 # Persist the setup scripts for boot, since it's not as simple to mount them into a QEMU VM
 cp -r $build_scripts_root /usr/lib/os-setup
-
-$build_scripts_root/ci-pre-headless-boot.sh
-# Don't make a login prompt on the main tty where setup script output will be shown:
-systemctl disable getty@tty1.service
