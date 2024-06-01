6c40a312746e60801631c4ad71da543f57edbc05
Ethan Li <lietk12@gmail.com>
2024-05-31 20:37:16 -0400

    Make a pallet for the demo (#4)
    * Make a pallet for the demo
    
    * Bump forklift version for CI checks
    
    * Cache repos for pallet CI check
    
    * Try to fix caching of repos in pallet CI check
    
    * Fix incorrect file name
    
    * Install hostapd as a dependency of `autohotspot`
    
    * Add hosts file to custom-domain package
    
    * Use hostname rather than machine-name for hostsfile
    
    * Add hostname template to the custom-domain package
    
    * Add cockpit config package
    
    * Add hostname with custom domain to allowed Cockpit origins
    
    * Keep the SSID constant even when hostname changes in the demo
    
    * Provide a missing hostapd template for constant SSID
    
    * Make the repo compatible with pallets at Forklift v0.7.2-alpha.3
    
    * Declare a network listener on port 80 for hello-world pkg
    
    * Add demo guide to `README.md`

---
+++ os-setup/setup/networking/install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -eux
+# The networking configuration enables access to network services via static IP over Ethernet, and
+# via self-hosted wireless AP when a specified external wifi network is not available.
+
+# Determine the base path for copied files
+config_files_root=$(dirname $(realpath $BASH_SOURCE))
+
+# Install dependencies
+sudo apt-get install -y firewalld dnsmasq hostapd avahi-utils
+
+# By default hostapd.service is masked and enabled (which causes two symlinks to exist), which
+# prevents Forklift from being able to disable hostapd via a filesystem overlay. We override this by
+# manually removing those symlinks by default, since our autohotspot relies on hostapd being
+# unmaske and disabled.
+sudo systemctl unmask hostapd.service
+sudo systemctl disable hostapd.service
+
+# Disable firewalld for now
+# FIXME: enable firewalld and set up firewall rules
+if sudo systemctl disable firewalld.service --now 2>/dev/null; then
+  # Restart docker to integrate with firewalld
+  sudo systemctl restart docker
+else
+  # We can't stop it because we're not booted, so we don't need to stop it or restart Docker:
+  sudo systemctl disable firewalld.service
+fi
