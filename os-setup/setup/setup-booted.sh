033266407e44308212deb4c79a01a15ff44bb466
Ethan Li <lietk12@gmail.com>
2024-06-04 10:06:10 -0700

    Print docker/containerd service logs for troubleshooting

--- os-setup/setup/setup-booted.sh
+++ os-setup/setup/setup-booted.sh
@@ -32,7 +32,7 @@ function panic {
 
 description="set up OS configuration with Forklift"
 report_starting "$description"
-if $build_scripts_root/forklift/setup-in-vm.sh ; then
+if $build_scripts_root/forklift/setup-booted.sh ; then
   report_finished "$description"
 else
   panic "$description"
